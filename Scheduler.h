650952d7cb27e2dfa5a85c5c3e8adb420de61521
print-Alonzo <alonzo_rimando@dlsu.edu.ph>
2025-06-15 15:08:07 +0800

    updated scheduler abstract class

--- Scheduler.h
+++ Scheduler.h
@@ -1,10 +1,27 @@
 #pragma once
-
 #include "Process.h"
 
+// Abstract base class for different CPU scheduling algorithms
 class Scheduler {
 public:
-	void addProcess(const Process* process);
-	void sortReadyQueue();
-	void runScheduler();
+    /**
+     * Enqueue a new process into the scheduler.
+     * @param process Pointer to the process to schedule.
+     */
+    virtual void addProcess(const Process* process) = 0;
+
+    /**
+     * Start the scheduling mechanism (spawn threads, begin dispatching).
+     */
+    virtual void start() = 0;
+
+    /**
+     * Stop scheduling, wait for all tasks to complete, and clean up threads.
+     */
+    virtual void stop() = 0;
+
+    /**
+     * Virtual destructor to allow proper cleanup of derived schedulers.
+     */
+    virtual ~Scheduler() = default;
 };
